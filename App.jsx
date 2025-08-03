5ffbe59a5dc4050809a340ae141cca9c356e2cbe
DANIELASUGG <danieladias894@gmail.com>
2025-08-03 19:44:08 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen text-center p-4">
+      <img src="https://cdn-icons-png.flaticon.com/512/64/64576.png" alt="Salto Alto" className="w-24 h-24 mb-6" />
+      <h1 className="text-5xl font-bold mb-4">Sugar Elite</h1>
+      <p className="text-lg max-w-md">Site Sugar Daddy 100% pago, sensual, seguro e sofisticado. Encontros com estilo e exclusividade.</p>
+    </div>
+  );
+}
+
+export default App;
